c5aeacf76ebc745a41801b7bfeabe2afd61a3e27
PavlyukVadim <pavlyuk.dev@gmail.com>
2018-12-16 23:09:27 +0200

    intial stock charts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import React from 'react';
+import { render } from 'react-dom';
+import Chart from './Chart';
+import { getData } from './utils'
+
+import { TypeChooser } from "react-stockcharts/lib/helper";
+
+class ChartComponent extends React.Component {
+  componentDidMount() {
+    getData().then(data => {
+      this.setState({ data })
+    })
+  }
+  render() {
+    if (this.state == null) {
+      return <div>Loading...</div>
+    }
+    return (
+      <TypeChooser>
+        {type => <Chart type={type} data={this.state.data} />}
+      </TypeChooser>
+    )
+  }
+}
+
+render(
+  <ChartComponent />,
+  document.getElementById("root")
 );
